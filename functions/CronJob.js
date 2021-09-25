4d58ea5a861221c32548680e6e3e76f48b0dde82
dong <s8809880988@gmail.com>
2021-09-25 13:39:03 +0800

    update

--- functions/CronJob.js
+++ functions/CronJob.js
@@ -3,36 +3,47 @@ const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 const redis = require("redis");
 const dotenv = require('dotenv').config()
+const Logger = require('./Logger')
 
 module.exports = function() {
 
     // 每天早上 9 點抓資料
     const getTrends = new CronJob('0 0 9 * * *', async function() {
 
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto('https://trends.google.com.tw/trends/?geo=TW');
-        await page.waitForSelector('.list-item-title')
+        try {
+            
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto('https://trends.google.com.tw/trends/?geo=TW');
+            await page.waitForSelector('.list-item-title')
 
-        let body = await page.content()
+            let body = await page.content()
 
-        let $ = await cheerio.load(body)
+            let $ = await cheerio.load(body)
 
-        let titles = [];
+            let titles = [];
 
-        await $('.list-item-title').each(function(idx, el) {
-            titles.push($(this).text());
-        })
+            await $('.list-item-title').each(function(idx, el) {
+                titles.push($(this).text());
+            })
 
-        // console.log(titles)
+            // console.log(titles)
 
-        await browser.close();
+            await browser.close();
 
-        const client = redis.createClient({host: dotenv.parsed.APP_REDIS_HOST});
+            const client = redis.createClient({host: dotenv.parsed.APP_REDIS_HOST});
 
-        await client.set('google_trends', JSON.stringify(titles));
+            await client.set('google_trends', JSON.stringify(titles));
 
-        client.quit();
+            client.quit();
+
+        } catch (error) {
+
+            client.quit();
+
+            Logger.error('getTrends ' + error.toString())
+                
+        }
 
     }, null, true, 'Asia/Taipei');
 
